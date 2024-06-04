0eabf67429a4d37a9e7d250f596933870112dde8
narendira <narendira2701@gmail.com>
2024-06-04 17:07:18 +0530

    fix

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,13 +2,16 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { hashPassword, verifyHash } from "../hash";
-import { sign } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 import { signinInput, signupInput } from "@narendira/blog-common";
 
 const userRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT: string;
+  };
+  Variables: {
+    userId: string;
   };
 }>();
 
@@ -86,17 +89,227 @@ userRouter.post("/signin", async (c) => {
   }
 });
 
-// userRouter.delete("/delete", async (c) => {
-//   const prisma = new PrismaClient({
-//     datasourceUrl: c.env.DATABASE_URL,
-//   }).$extends(withAccelerate());
+userRouter.use("/*", async (c, next) => {
+  const header = c.req.header("authorization");
+  if (!header) {
+    c.status(401);
+    return c.json({ msg: "Jwt token not found" });
+  }
+  const authToken = header.split(" ")[1];
+  if (!authToken) {
+    c.status(401);
+    return c.json({ msg: "Invalid Request" });
+  }
+  try {
+    const value = await verify(authToken, c.env.JWT);
+    c.set("userId", value.id);
+    await next();
+  } catch {
+    c.status(401);
+    return c.json({ msg: "Invalid Jwt" });
+  }
+});
 
-//   try {
-//     const deleteUser = await prisma.user.deleteMany({});
-//     return c.json({ msg: "done", deletemsg: deleteUser });
-//   } catch (e: any) {
-//     console.log(e.message);
-//   }
-// });
+userRouter.get("/me", async (c) => {
+  c.status(200);
+  return c.json({ verified: true });
+});
+
+userRouter.get("/profile", async (c) => {
+  const authId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const getProfileDetails = await prisma.user.findFirst({
+      where: {
+        id: authId,
+      },
+    });
+    if (!getProfileDetails) {
+      return c.json({ msg: "User Details not found" });
+    }
+    const { name, title, description } = getProfileDetails;
+    return c.json({ details: { name, title, description } });
+  } catch (e) {
+    c.status(400);
+    return c.json({ msg: "Bad Request" });
+  }
+});
+
+userRouter.put("/profile", async (c) => {
+  const authId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  try {
+    const updateProfile = await prisma.user.update({
+      where: {
+        id: authId,
+      },
+      data: {
+        name: body.name,
+        title: body.title,
+        description: body.description,
+      },
+    });
+    c.status(200);
+    return c.json({ updated: true });
+  } catch (e) {
+    c.status(400);
+    return c.json({ msg: "Bad Request" });
+  }
+});
+
+userRouter.get("/published", async (c) => {
+  const authId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const publishedBlog = await prisma.post.findMany({
+      where: {
+        authorId: authId,
+        published: true,
+      },
+      select: {
+        id: true,
+        created_at: true,
+        title: true,
+        content: true,
+        published: true,
+        author: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+
+    const hiddenBlogs = await prisma.post.findMany({
+      where: {
+        authorId: authId,
+        published: false,
+      },
+      select: {
+        id: true,
+        created_at: true,
+        title: true,
+        content: true,
+        published: true,
+        author: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+
+    c.status(200);
+    return c.json({ publishedBlog, hiddenBlogs });
+  } catch (e) {
+    c.status(400);
+    return c.json({ msg: "Bad Request" });
+  }
+});
+
+userRouter.post("/save", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const authId = c.get("userId");
+  const body = await c.req.json();
+
+  try {
+    const res = await prisma.savedPost.create({
+      data: {
+        postId: body.postId,
+        userId: authId,
+      },
+    });
+    return c.json({ msg: res });
+  } catch (e) {
+    c.status(400);
+    return c.json({ msg: "Bad Request" });
+  }
+});
+
+userRouter.get("/save", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const authId = c.get("userId");
+
+  try {
+    const res = await prisma.savedPost.findMany({
+      where: {
+        userId: authId,
+        post: {
+          published: true,
+        },
+      },
+      select: {
+        id: true,
+        postId: true,
+        post: {
+          select: {
+            id: true,
+            title: true,
+            content: true,
+            created_at: true,
+          },
+        },
+      },
+    });
+
+    return c.json({ data: res });
+  } catch (e) {
+    c.status(400);
+    return c.json({ msg: "Bad Request" });
+  }
+});
+
+userRouter.delete("/save", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const dbres = await prisma.savedPost.delete({
+      where: {
+        id: body.id,
+      },
+    });
+    return c.json({ msg: "success" });
+  } catch (e) {
+    c.status(400);
+    return c.json({ msg: "Bad Request" });
+  }
+});
+
+userRouter.delete("/delete", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const authId = c.get("userId");
+
+  try {
+    const deleteUser = await prisma.user.delete({
+      where: {
+        id: authId,
+      },
+    });
+    return c.json({ msg: "User Deleted" });
+  } catch (e) {
+    c.status(400);
+    return c.json({ msg: "Bad Request" });
+  }
+});
 
 export default userRouter;
